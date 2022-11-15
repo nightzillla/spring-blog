d5a974ad62a9b343c7d3ba573e30fdfbb92be3c3
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-15 13:37:11 -0600

    initialize repo

---
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    // telling spring framework to do get method
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
