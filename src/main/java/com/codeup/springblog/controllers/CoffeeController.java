325edccdfc8059bcddb228434c43002ddd13cf8f
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-16 11:52:58 -0600

    learning thymeleaf

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,7 +1,9 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +12,12 @@ public class CoffeeController {
 
     @GetMapping
     public String coffee(){
+        return "coffee";
+    }
+
+    @GetMapping("/{roast}")
+    public String roast(@PathVariable String roast, Model model){
+        model.addAttribute("roast", roast);
         return "coffee";
     }
 
