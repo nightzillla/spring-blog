8cc5c6266c6a2fcbb383d6d827c9fa07d415f361
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-16 17:20:36 -0600

    finshed roll dice

--- src/main/java/com/codeup/springblog/controllers/Roll_DiceController.java
+++ src/main/java/com/codeup/springblog/controllers/Roll_DiceController.java
@@ -1,14 +1,17 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-@RequestMapping("/roll-dice/n")
 public class Roll_DiceController {
 
-    int roll() {
-        return (int)(6.0 * Math.random()) + 1;
+    @GetMapping(path = "/roll-dice/{num}")
+    public String roll(@PathVariable int num, Model model) {
+        model.addAttribute("randomNumber",(int)(6.0 * Math.random()) + 1);
+        return "roll_Dice";
     }
 
 }
