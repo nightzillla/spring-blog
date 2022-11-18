62c8e2070cf94b4ac9cf88724f72e31e3b54e0df
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-18 11:02:07 -0600

    finshed exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,23 +1,38 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
+import com.codeup.springblog.models.Post;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/post")
 public class PostController {
 
-        @GetMapping
-        @ResponseBody
-        public String allpPosts() {
-            return "post index page";
+        @GetMapping("/")
+//        @ResponseBody
+        public String allPosts(Model model) {
+            Post post1 = new Post("all Posts","I am testing for title");
+            Post post2 = new Post("all Body","I am testing for body");
+            List<Post> allPostsList = new ArrayList<>(List.of(post1, post2));
+            model.addAttribute("allPosts",allPostsList);
+            return "/posts/show";
         }
 
         @GetMapping("/{id}")
-        @ResponseBody
-        public String onePost(@PathVariable long id) {
-            return "view an individual post " + id;
+//        @ResponseBody
+        public String onePost(@PathVariable int id, Model model) {
+            Post singlePost = new Post ("My Title", "My Body");
+            model.addAttribute("aPost", singlePost);
+            return "/posts/show";
         }
+//        return "view an individual post " + id;
+//    }
 
         @GetMapping("/posts/create")
         @ResponseBody
--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -2,8 +2,30 @@ package com.codeup.springblog.models;
 
 public class Post {
 
-   public void getTitle(){}
+    private String title;
+    private String body;
 
-   public void setTitle(){}
+   public String getTitle(){
+       return title;
+   }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public  String getBody(){
+       return body;
+   }
+    public void setBody(){}
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    public Post (){
+    }
+    public Post (String title, String body){
+       this.title = title;
+       this.body = body;
+    }
 
 }
