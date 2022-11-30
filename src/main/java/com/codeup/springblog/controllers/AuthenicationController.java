629a69fc6379e9a4143009dded267007f310e0dc
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-30 16:15:13 -0600

    starting login menu

---
+++ src/main/java/com/codeup/springblog/controllers/AuthenicationController.java
@@ -0,0 +1,13 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenicationController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "/login";
+    }
+}
