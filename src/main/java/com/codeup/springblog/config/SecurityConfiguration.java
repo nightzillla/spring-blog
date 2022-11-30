7f9a252c438e7fccc52a72787e9252d55a76d03a
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-11-30 17:40:06 -0600

    editing code for security

--- src/main/java/com/codeup/springblog/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/springblog/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.codeup.springblog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,13 +18,14 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests()
                 .antMatchers("/posts/create", "/posts/{id}/edit").authenticated()
                 .antMatchers("/", "/posts", "/posts/{id}").permitAll()
-                .and().formLogin()
+                .and().formLogin().loginPage("/login").defaultSuccessUrl("/")
+                .and().logout()
                 .and().httpBasic();
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private final UserRepository userDao;
-
-    public UserController(UserRepository userDao){
+    private final PasswordEncoder passwordEncoder;
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder){
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/register")
@@ -24,6 +26,7 @@ public class UserController {
     }
     @PostMapping("/register")
     public String registerUser(@ModelAttribute User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userDao.save(user);
         return "redirect:/posts/create";
     }
