0228ba60e0bfc04162861e34cc9956bff5e3b295
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-03 09:59:45 -0600

    updating spring blog

---
+++ src/main/resources/static/js/threejs.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+
+const renderer = new THREE.WebGL1Renderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement)
