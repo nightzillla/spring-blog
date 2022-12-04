cb72726f52c60cad3bd80ac9cab9aa9478b65d30
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-12-03 21:32:44 -0600

    updating code for iframe

--- src/main/resources/static/js/threejs.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-
-const renderer = new THREE.WebGL1Renderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement)
